056813c003f125d0eb9fe39d0d94efcafe0cbd51
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2025-05-11 04:24:36 +0300

    feat(entities-setup): set the relationships between Group, Post and User entities

--- src/main/java/com/minisocial/minisocialapi/entities/Group.java
+++ src/main/java/com/minisocial/minisocialapi/entities/Group.java
@@ -1,4 +1,32 @@
 package com.minisocial.minisocialapi.entities;
 
-public class Group {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Table(name="groups")
+@Entity
+public class Group implements Serializable {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+
+    @OneToMany(mappedBy = "group")
+    private Set<UserGroup> userGroups;
+
+
+    @OneToMany(mappedBy = "group")
+    private Set<Post> posts;
+
+    // need a manual logic to prevent duplication
+    @ManyToMany(mappedBy = "requestedGroups")
+    private Set<User> joinRequesters;
 }
